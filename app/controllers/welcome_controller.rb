9130a5aa6f97da2a88e9c91a0b078fa399ed6f77
Hugh Sutton-Gee <hsuttong@gmail.com>
2016-02-22 19:33:29 -0800

    Update gems, fix HTTParty config, add first api test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,13 @@
 class WelcomeController < ApplicationController
+  include HTTParty
   def index
+    puts "THIS IS JUST AT TEST:"
+    puts "GET ALL FOSSIL COLLECTIONS IN LAT/LONG BOX"
+    url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=165.0&lngmax=60.0&latmin=15.0&latmax=60.0&level=2"
+    pp HTTParty.post(url)
+
+    puts "DETAILED FOSSIL"
+    url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
+    pp HTTParty.post(url2)
   end
 end
-
-
-java ember and web servics
-
-java on top of scala
