b7cf14710b6d1fa25594384d27502667682146f8
Hugh Sutton-Gee <hsuttong@gmail.com>
2016-03-03 16:58:23 -0800

    -Update Gems -Remove welcome coffee script. -Fix googleMaps script include. Mv from application template to welcome view (essential our index) -Update LatLng box for querying api, so we only get USA. -fossil.js: clean up script, remove some debugging and console.logs, disable detailed view for now.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,32 +1,30 @@
 class WelcomeController < ApplicationController
   include HTTParty
+    @@url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=-165.0&lngmax=-61.0&latmin=24.0&latmax=60.0&level=2"
+    @@url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
+
   def index
+    # url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=-165.0&lngmax=-61.0&latmin=24.0&latmax=60.0&level=2"
+    # url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
     puts "THIS IS JUST AT TEST:"
     puts "GET ALL FOSSIL COLLECTIONS IN LAT/LONG BOX"
-    url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=165.0&lngmax=60.0&latmin=15.0&latmax=60.0&level=2"
-    pp HTTParty.post(url)
+    pp HTTParty.post(@@url)
 
-    puts "DETAILED FOSSIL"
-    url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
-    pp HTTParty.post(url2)
+    # puts "DETAILED FOSSIL"
+    # pp HTTParty.post(@@url2)
   end
 
   def getList
+    # url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=-165.0&lngmax=-61.0&latmin=24.0&latmax=60.0&level=2"
 
     p "hit getList"
-
-    url = "https://paleobiodb.org/data1.2/colls/summary.json?lngmin=165.0&lngmax=60.0&latmin=15.0&latmax=60.0&level=2"
-    entire_list = HTTParty.post(url)
-
+    entire_list = HTTParty.post(@@url)
     render json: entire_list.to_json
-
   end
 
   def getDetail
-    url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
-    detail = HTTParty.post(url2)
-
+    # url2 = "https://paleobiodb.org/data1.2/colls/single.json?id=2000070&show=full"
+    detail = HTTParty.post(@@url2)
     render json: detail.to_json
   end
 end
-#this is a test change
